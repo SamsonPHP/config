e0a6c659a479df63a00d4ba32a6508c06939c667
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-01-13 17:37:41 +0200

    Update samson.php

--- src/samson.php
+++ src/samson.php
@@ -5,5 +5,5 @@
 
 //[PHPCOMPRESSOR(remove,start)]
 // Subscribe to core started event to load all possible module configurations
-\samsonphp\event\Event::subscribe('core.configure', array(new \samsonos\config\Manager(), 'init'));
+\samsonphp\event\Event::subscribe('core.configure', array(new \samsonphp\config\Manager(), 'init'));
 //[PHPCOMPRESSOR(remove,end)]
